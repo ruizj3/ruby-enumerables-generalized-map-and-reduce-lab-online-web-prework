66753c70e737c9e2fcffe8b1a0583cf70538609f
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-15 03:03:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(string)
+  new_array = []
+  index = 0
+  while index < string.length
+    new_array.push(yield(string[index]))
+    index += 1
+  end
+  new_array
+end
+
+def reduce(string, starting_point=nil)
+  if starting_point
+    sum = starting_point
+    index = 0
+  else
+    sum = string[0]
+    index = 1
+  end
+  while index < string.length
+    sum = yield(sum, string[index])
+    index += 1
+  end
+  sum
+end
